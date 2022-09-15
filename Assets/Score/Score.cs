c90df56d671ed2873f58a0a0b65549f1d412bd92
Conner M <connermullins00@gmail.com>
2022-09-15 15:56:36 -0700

    Moved the location of the score text and finished setting up the hay item. The score script has been finished as well, so hay can now be collected by the player and will update the score text accordingly.

--- Assets/Score/Score.cs
+++ Assets/Score/Score.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    // String that will be displayed
     public Text MyscoreText;
+
+    // Total points
     private int ScoreNum;
 
     // The score is initially set to 0 and is displayed as such.
@@ -14,18 +17,20 @@ public class Score : MonoBehaviour
         ScoreNum = 0;
         MyscoreText.text = "Score: " + ScoreNum;
     }
-
-    // This portion is commented out until 
-
-    /*
-    // When the player collides with the hay, the score will update and the hay object will be destroyed.
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+    // When the player collides with the hay the score will update and the hay object will be destroyed.
+    private void OnTriggerEnter2D(Collider2D Hay)
     {
         if(Hay.tag == "MyHay")
         {
+            // Update score
             ScoreNum += 100;
+
+            // Remove hay object
             Destroy(Hay.gameObject);
-            MyscoreText.text = "Score" + ScoreNum;
+
+            // Update text that is displayed
+            MyscoreText.text = "Score: " + ScoreNum;
         }
-    } */
+    }
 }
